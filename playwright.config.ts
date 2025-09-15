425b30c6c376d2b5170495e5d4435cc1bdfbb250
Oleksandr Shevchuk <oleksandr.shevchuk@sitecore.com>
2025-09-15 13:53:53 +0300

    update reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+const config: PlaywrightTestConfig = {
   testDir: './ui-tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -21,8 +21,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -77,4 +76,23 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+};
+
+/* Reporter to use. See https://playwright.dev/docs/test-reporters */
+if (!process.env.CI) {
+  config.reporter = [['html', { open: 'never' }], ['line']];
+} else {
+  config.reporter = [
+    ['html', { open: 'never' }],
+    [
+      '@estruyf/github-actions-reporter',
+      {
+        title: 'Test report',
+        useDetails: true,
+        showError: true,
+      },
+    ],
+  ];
+}
+
+export default config;
